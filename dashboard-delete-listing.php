0201a45c0735782649e9ee6d9d184b0eef147230
alimoonpk <alimoon.pk@gmail.com>
2017-08-22 17:11:34 +0500

    Version 2
    Update and Delete Functions

---
+++ dashboard-delete-listing.php
@@ -0,0 +1,385 @@
+
+    <?php require_once("Include/DB.php"); ?>
+    <?php require_once("Include/Sessions.php"); ?>
+    <?php require_once("Include/Functions.php"); ?>
+    <?php Confirm_Login(); ?>
+    <?php
+    global $ConnectingDB;
+	$DeleteFromURL=$_GET['Delete'];
+	$Query="DELETE FROM photographerdata WHERE id='$DeleteFromURL'";
+	
+	$Execute=mysql_query($Query);
+	
+	if($Execute){
+	$_SESSION["SuccessMessage"]="Listing Deleted Successfully";
+	Redirect_to("Dashboard.php");
+	}else{
+	$_SESSION["ErrorMessage"]="Something Went Wrong. Try Again !";
+	Redirect_to("Dashboard.php");
+		
+	}
+	
+	
+	
+
+
+    ?>
+
+    <!DOCTYPE html>
+    <head>
+
+    <!-- Basic Page Needs
+    ================================================== -->
+    <title>Photographers Directory</title>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1, maximum-scale=1">
+
+    <!-- CSS
+    ================================================== -->
+    <link rel="stylesheet" href="css/style.css">
+    <link rel="stylesheet" href="css/colors/main.css" id="colors">
+
+    </head>
+
+    <body>
+
+    <!-- Wrapper -->
+    <div id="wrapper">
+
+    <!-- Header Container
+    ================================================== -->
+    <header id="header-container" class="fixed fullwidth dashboard">
+
+        <!-- Header -->
+        <div id="header" class="not-sticky">
+            <div class="container">
+
+                <!-- Left Side Content -->
+                <div class="left-side">
+
+                    <!-- Logo -->
+                    <div id="logo">
+                        <a href="index.php"><img src="images/logo.png" alt=""></a>
+                        <a href="index.php" class="dashboard-logo"><img src="images/logo.png" alt=""></a>
+                    </div>
+
+                    <!-- Mobile Navigation -->
+                    <div class="menu-responsive">
+                        <i class="fa fa-reorder menu-trigger"></i>
+                    </div>
+
+                    <!-- Main Navigation -->
+                    <nav id="navigation" class="style-1">
+                        <ul id="responsive">
+
+                            <li><a href="index.php">Home</a>
+
+                            </li>
+
+                            <li><a href="listings-list-full-width.php">Listings</a>
+
+                            </li>
+
+                            <li><a class="current" href="#">User Panel</a>
+                                <ul>
+                                    <li><a href="dashboard.php">Dashboard</a></li>
+                                    <li><a href="dashboard-add-listing.php">Add Listing</a></li>
+                                </ul>
+                            </li>
+
+                            <li><a href="#">Contact
+
+                            </a>
+
+                            </li>
+
+                        </ul>
+                    </nav>
+                    <div class="clearfix"></div>
+                    <!-- Main Navigation / End -->
+
+                </div>
+                <!-- Left Side Content / End -->
+
+                <!-- Right Side Content / End -->
+                <div class="right-side">
+                    <!-- Header Widget -->
+                    <div class="header-widget">
+
+                        <!-- User Menu -->
+                        <div class="user-menu">
+                            <div class="user-name"><span><img src="images/furqan.jpg" alt="" height="45" width="65"></span>Ali Moon</div>
+                            <ul>
+                                <li><a href="dashboard.php"><i class="sl sl-icon-settings"></i> Dashboard</a></li>
+                             
+                                <li><a href="dashboard-add-listing.php"><i class="sl sl-icon-settings"></i> Add Listing</a></li>
+                                <li><a href="Logout.php"><i class="sl sl-icon-power"></i> Logout</a></li>
+                            </ul>
+                        </div>
+
+                        <a href="dashboard-add-listing.php" class="button border with-icon">Add Listing <i class="sl sl-icon-plus"></i></a>
+                    </div>
+                    <!-- Header Widget / End -->
+                </div>
+                <!-- Right Side Content / End -->
+
+            </div>
+        </div>
+        <!-- Header / End -->
+
+    </header>
+    <div class="clearfix"></div>
+    <!-- Header Container / End -->
+
+
+    <!-- Dashboard -->
+    <div id="dashboard">
+
+        <!-- Navigation
+        ================================================== -->
+
+        <!-- Responsive Navigation Trigger -->
+        <a href="#" class="dashboard-responsive-nav-trigger"><i class="fa fa-reorder"></i> Dashboard Navigation</a>
+
+        <div class="dashboard-nav">
+
+            <ul data-submenu-title="Main">
+                <li><a href="dashboard.php"><i class="sl sl-icon-settings"></i> Dashboard</a></li>
+                <li><a href="dashboard-add-listing.php"><i class="sl sl-icon-layers"></i>Add New Listing</a></li>
+            </ul>
+
+           	
+
+            <ul data-submenu-title="Account">
+                <li><a href="Logout.php"><i class="sl sl-icon-power"></i> Logout</a></li>
+            </ul>
+
+        </div>
+        <!-- Navigation / End -->
+
+
+        <!-- Content
+        ================================================== -->
+        <div class="dashboard-content">
+
+            <!-- Titlebar -->
+            <div id="titlebar">
+                <div class="row">
+                    <div class="col-md-12">
+                        <h2>Add Listing</h2>
+                        <!-- Breadcrumbs -->
+                        <nav id="breadcrumbs">
+                            <ul>
+                                <li><a href="#">Home</a></li>
+                                <li><a href="#">Dashboard</a></li>
+                                <li>Add category</li>
+                            </ul>
+                        </nav>
+                    </div>
+                </div>
+            </div>
+
+            <div class="row">
+                <div class="col-lg-12">
+
+                    <div id="add-listing">
+
+                        <!-- Section -->
+                        <div class="add-listing-section">
+                           
+<form action="dashboard-add-listing.php" method="post" enctype="multipart/form-data">
+                            <!-- Headline -->
+    
+                            <div class="add-listing-headline">
+                                <h3><i class="sl sl-icon-doc"></i> Basic Informations</h3>
+                            </div>
+
+                            <!-- Title -->
+                            <div class="row with-forms">
+                                <div class="col-md-12">
+                                    <h5>Studio Name <i class="tip" data-tip-content="Name of your business"></i></h5>
+                                    <input name="Studioname" class="search-field" type="text" value=""/>
+                                    <h5>Owner Name</h5>
+                                    <input name="Personname" class="search-field" type="text" value=""/>
+                                </div>
+                            </div>
+
+                            <!-- Row -->
+                            <div class="row with-forms">
+
+                                <!-- Status -->
+                                <div class="col-md-6">
+                                    <h5>Category</h5>
+
+
+
+
+
+                                    <select name="Category" class="chosen-select-no-single" >
+
+                                        <?php
+    global $ConnectingDB;
+    $ViewQuery="SELECT * FROM category ORDER BY id desc";
+    $Execute=mysql_query($ViewQuery);
+    while($DataRows=mysql_fetch_array($Execute)){
+        $Id=$DataRows["id"];
+        $CategoryName=$DataRows["name"];
+    ?>	
+        <option><?php echo $CategoryName; ?></option>
+        <?php } ?>
+
+                                    </select>
+                                </div>
+
+                               
+
+                            </div>
+                            <!-- Row / End -->
+
+                            </div>
+                        <!-- Section / End -->
+
+                      
+
+
+                        <!-- Section -->
+                        <div class="add-listing-section margin-top-45">
+
+                            <!-- Headline -->
+                            <div class="add-listing-headline">
+                                <h3><i class="sl sl-icon-picture"></i> Gallery (Choose your 4 Best Images)</h3>
+                            </div>
+
+                            <!-- Dropzone -->
+                            <div class="submit-section">
+                                <input type="File" class="form-control" name="Image1" id="imageselect">
+                                <input type="File" class="form-control" name="Image2" id="imageselect">
+                                <input type="File" class="form-control" name="Image3" id="imageselect">
+                                <input type="File" class="form-control" name="Image4" id="imageselect">
+                            </div>
+
+                        </div>
+                        <!-- Section / End -->
+
+
+                        <!-- Section -->
+                        <div class="add-listing-section margin-top-45">
+
+                            <!-- Headline -->
+                            <div class="add-listing-headline">
+                                <h3><i class="sl sl-icon-docs"></i> Details</h3>
+                            </div>
+
+                            <!-- Description -->
+                            <div class="form">
+                                <h5>Description</h5>
+                                <textarea name="Overview" class="WYSIWYG" name="summary" cols="40" rows="3" id="summary" spellcheck="true"></textarea>
+                            </div>
+
+                            <!-- Row -->
+                            <div class="row with-forms">
+
+                                <!-- Phone -->
+                                <div class="col-md-4">
+                                    <h5>Phone <span>(optional)</span></h5>
+                                    <input name="Cell" type="text">
+                                </div>
+
+                                <!-- Website -->
+                                <div class="col-md-4">
+                                    <h5>Website <span>(optional)</span></h5>
+                                    <input name="Web" type="text">
+                                </div>
+
+                                <!-- Email Address -->
+                                <div class="col-md-4">
+                                    <h5>E-mail <span>(optional)</span></h5>
+                                    <input name="Email" type="text">
+                                </div>
+
+                            </div>
+                            <!-- Row / End -->
+
+
+                            <!-- Row -->
+                            <div class="row with-forms">
+
+                                <!-- Phone -->
+                                <div class="col-md-6">
+                                    <h5 class="fb-input">Pricing Per Picture <span>(Photoshoot)</span></h5>
+                                    <input name="Pricingpp" type="text">
+                                </div>
+
+                                <!-- Website -->
+                                <div class="col-md-6">
+                                    <h5 class="twitter-input">Pricing Per Hour<span>(Video)</span></h5>
+                                    <input name="Pricingph" type="text">
+                                </div>
+
+                              
+                            </div>
+                            <!-- Row / End -->
+
+
+
+                        </div>
+                        <!-- Section / End -->
+
+
+                           
+
+                        </div>
+                        <!-- Section / End -->
+
+
+                        
+                        <input class="button preview" type="Submit" name="Submit" value="Add New Listing">
+
+                    </div>
+                </div>
+
+                </form>
+                
+                <!-- Copyrights -->
+                <div class="col-md-12">
+                    <div class="copyrights">© 2017 Best Photography. All Rights Reserved.</div>
+                </div>
+
+            </div>
+
+        </div>
+        <!-- Content / End -->
+
+
+    </div>
+    <!-- Dashboard / End -->
+
+
+    </div>
+    <!-- Wrapper / End -->
+
+
+    <!-- Scripts
+    ================================================== -->
+    <script type="text/javascript" src="scripts/jquery-2.2.0.min.js"></script>
+    <script type="text/javascript" src="scripts/jpanelmenu.min.js"></script>
+    <script type="text/javascript" src="scripts/chosen.min.js"></script>
+    <script type="text/javascript" src="scripts/slick.min.js"></script>
+    <script type="text/javascript" src="scripts/rangeslider.min.js"></script>
+    <script type="text/javascript" src="scripts/magnific-popup.min.js"></script>
+    <script type="text/javascript" src="scripts/waypoints.min.js"></script>
+    <script type="text/javascript" src="scripts/counterup.min.js"></script>
+    <script type="text/javascript" src="scripts/jquery-ui.min.js"></script>
+    <script type="text/javascript" src="scripts/tooltips.min.js"></script>
+    <script type="text/javascript" src="scripts/custom.js"></script>
+
+
+   
+
+    <!-- DropZone | Documentation: http://dropzonejs.com -->
+    <script type="text/javascript" src="scripts/dropzone.js"></script>
+
+
+    </body>
+
+    </html>
